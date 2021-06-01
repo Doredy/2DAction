979cab952429e039c0c01a7b627770d592186ad8
Doredy <yui242628@gmail.com>
2021-06-01 11:51:57 +0900

    7章まで 挙動不安定

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,82 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private string horizontal = "Horizontal";
+    private string jump = "Jump";
+    private Rigidbody2D rb;
+    private float scale;
+    private Animator anim;
+    public float moveSpeed;
+    public float jumpPower;
+
+    public bool isGrounded;
+    [SerializeField]
+    private LayerMask groundLayer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        scale = transform.localScale.x;
+        anim = GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+        isGrounded = Physics2D.Linecast(transform.position + transform.up * 0.4f, transform.position - transform.up * 0.9f, groundLayer);
+        Debug.DrawLine(transform.position + transform.up * 0.4f, transform.position - transform.up * 0.9f, Color.red, 1.0f);
+
+        if (Input.GetButtonDown(jump))
+        {
+            Jump();
+        }
+        if (isGrounded == false && rb.velocity.y < 0.15f)
+        {
+            anim.SetTrigger("Fall");
+        }
+    }
+
+    private void Jump()
+    {
+        rb.AddForce(transform.up * jumpPower);
+        anim.SetTrigger("Jump");
+    }
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        Move();
+    }
+
+    private void Move()
+    {
+        float x = Input.GetAxis(horizontal);
+
+        if (x != 0)
+        {
+            rb.velocity = new Vector2(x * moveSpeed, rb.velocity.y);
+            //@H
+            Vector3 temp = transform.localScale;
+            temp.x = x;
+            if (temp.x > 0)
+            {
+                temp.x = scale;
+            }
+            else
+            {
+                temp.x = -scale;
+            }
+            transform.localScale = temp;
+            anim.SetBool("Idle", false);
+            anim.SetFloat("Run", 0.5f);
+        }
+        else
+        {
+            rb.velocity = new Vector2(0, rb.velocity.y);
+            anim.SetFloat("Run", 0.0f);
+            anim.SetBool("Idle", true);
+        }
+    }
+}
